4a629e7477e6ff2ea32ef89851afca72c6046c58
Kara Dmitry <karadmitrii@gmail.com>
2019-12-24 13:24:33 +0300

    Final version

--- docker.sh
+++ docker.sh
@@ -12,4 +12,4 @@ docker &&
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 docker-compose --version &&
-docker ps &&
+docker ps
