aa7fd1bde733d3f2877d2b8f8cbe636e0d1e6f9b
Kara Dmitry <karadmitrii@gmail.com>
2020-08-05 02:42:50 +0300

    Update docker-compose version

--- docker.sh
+++ docker.sh
@@ -9,7 +9,7 @@ sudo apt update &&
 apt-cache policy docker-ce &&
 sudo apt install docker-ce &&
 docker &&
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose &&
+sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 docker-compose --version &&
 docker ps
