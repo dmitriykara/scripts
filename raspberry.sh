03fcb6b8ed94c3c2299606cedf7be93a3583c270
Dmitriy Kara <karadmitrii@gmail.com>
2021-04-20 01:43:42 +0300

    Update raspberry.sh

--- raspberry.sh
+++ raspberry.sh
@@ -13,3 +13,17 @@ sudo apt-get upgrade -y;
 # wpa_passphrase "<Wi-Fi ESSID>" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null;
 # wpa_cli -i wlan0 reconfigure;
 # ifconfig wlan0;
+
+# Install golang
+# export GOLANG="$(curl https://golang.org/dl/|grep armv6l|grep -v beta|head -1|awk -F\> {'print $3'}|awk -F\< {'print $1'})"
+# wget https://golang.org/dl/$GOLANG
+# sudo tar -C /usr/local -xzf $GOLANG
+# rm $GOLANG
+# unset GOLANG
+
+#nano ~/.profile
+# Scroll all the way down to the end of the file and add the following:
+# PATH=$PATH:/usr/local/go/bin
+# GOPATH=$HOME/golang
+# source ~/.profile
+
