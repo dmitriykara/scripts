b54f2b44211808f49ad93dae14cb813f123c6922
Dmitriy Kara <karadmitrii@gmail.com>
2021-04-20 01:08:37 +0300

    Update raspberry.sh

--- raspberry.sh
+++ raspberry.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+passwd;
+
+# Enable ssh by key from outer device
+# ssh-copy-id <username (usually "pi")>@<host> # From outer device
+
 sudo apt-get update;
 sudo apt-get upgrade -y;
 
 # Change lang in "sudo raspi-config" -> "Localisation Options" -> "Change wireless country"
-sudo iwlist wlan0 scan; # Get list of Wi-Fi
-wpa_passphrase "<Wi-Fi ESSID>" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null;
-wpa_cli -i wlan0 reconfigure;
-ifconfig wlan0;
+# sudo iwlist wlan0 scan; # Get list of Wi-Fi
+# wpa_passphrase "<Wi-Fi ESSID>" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null;
+# wpa_cli -i wlan0 reconfigure;
+# ifconfig wlan0;
